92dea9ebcc2d1ab19235790adb4b1032798ef277
gergyemartin <martin.gergye@stud.uni-corvinus.hu>
2024-03-14 16:38:44 +0100

    2.commit

--- Hajosteszt/Form1.Designer.cs
+++ Hajosteszt/Form1.Designer.cs
@@ -28,19 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(1, 263);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(797, 185);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(21, 100);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(21, 147);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(492, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(264, 183);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private PictureBox pictureBox1;
     }
 }
--- Hajosteszt/Form1.cs
+++ Hajosteszt/Form1.cs
@@ -12,23 +12,30 @@ namespace Hajosteszt
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            OsszesKerdes = new List<Kerdes>();
+            OsszesKerdes = KerdesekBetoltse();
+            AktualisKerdesek = new List<Kerdes>();
 
+            for (int i = 0; i < 7; i++)
+            {
+                AktualisKerdesek.Add(OsszesKerdes[0]);
+                OsszesKerdes.RemoveAt(0);
+            }
+            dataGridView1.DataSource = AktualisKerdesek;
+            KerdesMegjelenites(AktualisKerdesek[MegjelenitettKerdesSzama]);
         }
         List<Kerdes> KerdesekBetoltse()
         {
             List<Kerdes> kerdesek = new List<Kerdes>();
-            OsszesKerdes = KerdesekBetoltse();
-            StreamReader sr = new StreamReader("hajos.txt",true);
+            StreamReader sr = new StreamReader("hajos.txt", true);
             while (!sr.EndOfStream)
             {
                 string sor = sr.ReadLine();
                 string[] sorok = sor.Split("\t");
                 Kerdes k = new Kerdes();
                 k.KerdesSzoveg = sorok[1];
-                k.Válasz1 = sorok[2];
-                k.Válasz2 = sorok[3];
-                k.Válasz3 = sorok[4];
+                k.Válasz1 = sorok[2].Trim('"');
+                k.Válasz2 = sorok[3].Trim('"');
+                k.Válasz3 = sorok[4].Trim('"');
                 k.URL = sorok[5];
                 int x = 0;
                 int.TryParse(sorok[6], out x);
@@ -36,8 +43,23 @@ namespace Hajosteszt
                 kerdesek.Add(k);
             }
             sr.Close();
-
             return kerdesek;
+            void KerdesMegjelenites(Kerdes kerdes)
+            {
+                label1.Text = kerdes.KerdesSzoveg;
+                textBox1.Text = kerdes.Válasz1;
+                textBox2.Text = kerdes.Válasz2;
+                textBox3.Text = kerdes.Válasz3;
+
+                if (string.IsNullOrEmpty(kerdes.URL))
+                {
+                    pictureBox1.Visible = false;
+                }
+                else
+                {
+                    pictureBox1.Load("https://storage.altinum.hu/hajo/" + kerdes.URL);
+                }
+            }
         }
     }
 }
